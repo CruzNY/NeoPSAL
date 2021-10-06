57433b75109a90b04892b78b5b54a7de6d9d6426
Alex Cruz <alexcruz182@gmail.com>
2021-10-05 20:59:18 -0400

    init commit, The start of something great?

---
+++ src/main/java/com/cruzny/neopsal/NeoPsalApplication.java
@@ -0,0 +1,13 @@
+package com.cruzny.neopsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoPsalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoPsalApplication.class, args);
+    }
+
+}
